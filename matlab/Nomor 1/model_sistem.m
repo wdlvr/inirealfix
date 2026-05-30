b4cf848f4ad90574ce96dfa654f9e3ccb6a28330
rafiihsanalfathin123 <16523076@std.stei.itb.ac.id>
2026-05-30 15:52:58 +0700

    Chore: Tambah komentar Nomor 1

--- matlab/Nomor 1/model_sistem.m
+++ matlab/Nomor 1/model_sistem.m
@@ -1,21 +1,18 @@
-% model_sistem.m
+% Nama / NIM : 
+% Rafi Ihsan Alfathin     / 13223018
+% Maghryza Milchan Fayumi / 13223036
+% William Anthony         / 13223048
+%% Deskripsi: Model sistem (fungsi transfer) yang akan dipakai untuk Tubes ini
 clear; clc; close all;
 
-%% Aircraft Pitch Control Transfer Function
+%% Fungsi transfer pitch control aircraft
 num = [1.151 0.1774];
 den = [1 0.739 0.921 0];
-
 G1 = tf(num, den);
 
-G1.InputName = 'delta_e';
-G1.OutputName = 'theta';
-
-sys_tf = G1;
-
+% Tampilkan fungsi transfer sistem
 disp('Aircraft pitch control transfer function system:');
-sys_tf
-
-disp('G1 = theta / delta_e:');
 G1
 
-save('aircraft_pitch_tf.mat', 'G1', 'sys_tf', 'num', 'den');
+% Simpan file .mat nya
+save('aircraft_pitch_tf.mat', 'G1', 'num', 'den');
